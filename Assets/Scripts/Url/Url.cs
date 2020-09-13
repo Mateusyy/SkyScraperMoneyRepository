cb9265c93c1ac9b9f1e3df4b2c6982ea2cd37b5f
Mateusyy <mtsz.grybos@gmail.com>
2020-09-13 09:28:26 +0200

    Discord invitation

---
+++ Assets/Scripts/Url/Url.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Url : MonoBehaviour
+{
+    public string urlAddress;
+
+    public void OpenUrl()
+    {
+        Application.OpenURL(urlAddress);
+    }
+}
